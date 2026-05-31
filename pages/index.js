c1aed12ad2651d53e760bb2c996d4ad97ec72740
Azlan Khan <azlank565@gmail.com>
2026-06-01 00:42:08 +0500

    feat: custom favicon

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home({
         <meta name="description" content="Premium skincare and haircare rooted in ancient wisdom." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+         <link rel="apple-touch-icon" href="/favicon.ico" />
       </Head>
 
       <Cursor />
